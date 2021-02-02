d11e2a46eb48c68110aacc6de262f04f9f574575
Dr. Aidoo <aidoojuniorsolomon@gmail.com>
2021-02-02 04:24:38 -0800

    Newest Commit for the day

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import {FaTimes} from 'react-icons/fa'
 
 const Task = ({task}) => {
     return (
         <div className="task">
-            <h3>{task.theTask}</h3>
+            <h3>{task.theTask} <FaTimes style={{color: 'blue', cursor:"pointer"}}/> </h3>
             <p>{task.day}</p>
         </div>
     )
